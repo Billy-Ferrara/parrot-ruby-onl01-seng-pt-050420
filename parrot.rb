84d85e42948089853beb81bedf8f0b08aebf1915
Billy Ferrara <willfira717@gmail.com>
2020-05-06 16:48:38 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,12 @@
-def parrot(phrase= "Pretty bird!")
-  if phrase = nil
-    puts "Squawk!"
-  else
-    puts "#{phrase}"
-  return "Squawk!"
-  end
-end
+# def parrot(phrase= "Pretty bird!")
+#   if phrase = nil
+#     puts "Squawk!"
+#   else
+#     puts "#{phrase}"
+#   return "Squawk!"
+#   end
+# end
+
+def parrot
+  puts "Squawk!"
+end 
